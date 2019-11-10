dd1a7dde4143071c7154f039eeafe8cb04826239
ChiefTwoPencils <robertwilk916@gmail.com>
2019-11-09 21:48:20 -0800

    fp level-up

--- csharp/CSharpFp/Battle/Program.cs
+++ csharp/CSharpFp/Battle/Program.cs
@@ -6,11 +6,13 @@ namespace Battle
     {
         static void Main(string[] args)
         {
-            Func<string, Func<string>> place = azz => () => $"Your {azz} done been {azz.ToLower()}-ed!";
+            Func<string, Func<string>> place = azz => () => $"Your {azz ?? "uh, oh!"} done been {azz?.ToLower() ?? "whoops"}-ed!";
             Func<string> placeFooAzz = place("FOO");
             Func<string> placeBarAzz = place("BAR");
+            Func<string> placeNullAzz = place(null);
             Console.WriteLine(placeFooAzz());
             Console.WriteLine(placeBarAzz());
+            Console.WriteLine(placeNullAzz());
         }
     }
 }
