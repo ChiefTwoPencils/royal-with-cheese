271c40bf46ea3e91bf5e5da0ba3d9021c8eda856
ChiefTwoPencils <robertwilk916@gmail.com>
2019-11-09 18:28:04 -0800

    strokes preserved

--- java/Battle/src/Main.java
+++ java/Battle/src/Main.java
@@ -2,7 +2,7 @@ import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-    	final Function<String, String> mi = (azz) -> {return "Your " + azz + " done been foo-ed!";};
+    	final Function<String, String> mi = azz -> "Your " + azz + " done been foo-ed!";
     	System.out.printf("%s\t%s", mi.apply("Not Today"), "Daaaammmmnnn!");
     }
 }
