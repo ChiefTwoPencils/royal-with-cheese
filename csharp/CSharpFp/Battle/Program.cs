d75b1671999308e007ce5f1ffa7ebad4fc43e52a
ChiefTwoPencils <robertwilk916@gmail.com>
2019-11-09 22:33:51 -0800

    last 1 4now

--- csharp/CSharpFp/Battle/Program.cs
+++ csharp/CSharpFp/Battle/Program.cs
@@ -6,11 +6,14 @@ namespace Battle
     {
         static void Main(string[] args)
         {
-            Func<string, Func<string>> place = azz => () => $"Your {azz ?? "uh, oh!"} done been {azz?.ToLower() ?? "whoops"}-ed!";
-            Func<string> placeFooAzz = place("FOO");
-            Func<string> placeBarAzz = place("BAR");
-            Func<string> placeNullAzz = place(null);
-            Console.WriteLine($"{placeFooAzz()}\n{placeBarAzz()}\n{placeNullAzz()}");
+            Func<string, Func<string, Func<string>>> place = state => azz => () 
+                => $"{state}: Your {azz ?? "uh, oh!"} done been {azz?.ToLower() ?? "whoops"}-ed!";
+            Func<string, Func<string>> good = place("Good");
+            Func<string, Func<string>> badd = place("Badd");
+            Func<string> goodFoo = good("FOO");
+            Func<string> goodBar = good("BAR");
+            Func<string> badNull = badd(null);
+            Console.WriteLine($"{goodFoo()}\n{goodBar()}\n{badNull()}");
         }
     }
 }
