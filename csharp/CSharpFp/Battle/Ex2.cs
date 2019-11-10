1835c40908e1c87785a9320cddf29725f2522fd3
ChiefTwoPencils <robertwilk916@gmail.com>
2019-11-10 13:04:58 -0800

    exercise 2.5 & 6

--- csharp/CSharpFp/Battle/Ex2.cs
+++ csharp/CSharpFp/Battle/Ex2.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 
-using BinaryOperator = System.Func<int, System.Func<int, int>>;
-
 namespace Battle
 {
+    public delegate Func<int, int> BinaryOperator(int i);
+
     public static class Ex2
     {
         public static readonly Func<int, int> Triple = a => a * 3;
         public static readonly Func<int, int> Square = a => a * a;
 
         public static Func<T, R> Compose<T, U, R>(Func<U, R> f, Func<T, U> g) => a => f(g(a));
+
+        public static Func<Func<T, R>, Func<Func<U, T>, Func<U, R>>> 
+        CurryCompose<T, U, R>() => f => g => a => f(g(a));
+
+        public static Func<Func<U, T>, Func<Func<T, R>, Func<U, R>>>
+        HigherAndThen<T, U, R>() => g => f => a => f(g(a));
 
         public static BinaryOperator Add = a => b => a + b;
         public static BinaryOperator Mul = a => b => a * b;
--- csharp/CSharpFp/Battle/Program.cs
+++ csharp/CSharpFp/Battle/Program.cs
@@ -21,14 +21,27 @@ namespace Battle
             list.ForEach(Console.WriteLine);
 
             // Exercise 2.1, 2.2
-            Console.WriteLine(Compose(Triple, Square)(5));
+            Console.WriteLine(Compose(Triple, Square)(5)); // 75
 
             // Exercise 2.3
             Console.WriteLine(Add(2)(3)); // 5
             var add2To = Add(2);
             Console.WriteLine(add2To(3)); // 5
+
             var multiply2By = Mul(2);
             Console.WriteLine(multiply2By(3)); // 6
+
+            BinaryOperator sub = a => b => a - b;
+            var from2Subtract = sub(2);
+            Console.WriteLine(from2Subtract(3)); // -1
+
+            // Exercise 2.5
+            var tripleSquare = CurryCompose<int, int, int>()(Triple)(Square);
+            Console.WriteLine(tripleSquare(5)); // 75
+
+            // Exercise 2.6
+            var tripleSquareFlipped = HigherAndThen<int, int, int>()(Square)(Triple);
+            Console.WriteLine(tripleSquareFlipped(5)); // 75
         }
     }
 }
