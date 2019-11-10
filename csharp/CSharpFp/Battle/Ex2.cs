9b3805a168262ed432410b69d01926e4c758dac7
ChiefTwoPencils <robertwilk916@gmail.com>
2019-11-10 10:51:35 -0800

    exercise 2.1

---
+++ csharp/CSharpFp/Battle/Ex2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Battle
+{
+    public static class Ex2
+    {
+        public static readonly Func<int, int> Triple = a => a * 3;
+        public static readonly Func<int, int> Square = a => a * a;
+
+        public static Func<T, R> Compose<T, U, R>(Func<U, R> f, Func<T, U> g) => a => f(g(a));
+
+        // static void Main() => Console.WriteLine(Compose(Triple, Square)(5));
+    }
+}
--- csharp/CSharpFp/Battle/Program.cs
+++ csharp/CSharpFp/Battle/Program.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using static Battle.Ex2;
+
 namespace Battle
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Ice breaker
             Func<string, Func<string, Func<string>>> place = state => azz => () 
                 => $"{state}: Your {azz ?? "uh, oh!"} done been {azz?.ToLower() ?? "whoops"}-ed!";
             Func<string, Func<string>> good = place("Good");
             Func<string, Func<string>> badd = place("Badd");
-            var list = new List<Func<string>>
+            var list = new List<string>
             {
-                good("FOO"), good("BAR"), badd(null)
+                good("FOO")(), good("BAR")(), badd(null)()
             };
-            list.ForEach(p => Console.Write($"{p()}\n"));
+            list.ForEach(Console.WriteLine);
+
+            // Exercise 2.1
+            Console.WriteLine(Compose(Triple, Square)(5));
         }
     }
 }
