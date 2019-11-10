99f5be0eeb4844c63d60fe63bc831c8abdc2c065
ChiefTwoPencils <robertwilk916@gmail.com>
2019-11-09 16:34:49 -0800

    string Foo xt

---
+++ csharp/CSharpFp/Battle/Extensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Battle
+{
+    public static class Extensions
+    {
+        public static string Foo(this string ztring) => $"Your {ztring} done been Foo-ed!";
+    }
+}
--- csharp/CSharpFp/Battle/Program.cs
+++ csharp/CSharpFp/Battle/Program.cs
@@ -6,7 +6,7 @@ namespace Battle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{"ass".Foo()}\tDaaaaaammmmnnnn!!!");
         }
     }
 }
